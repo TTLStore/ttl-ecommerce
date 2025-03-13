6db9fecd00e69707afaf4bc84f3697195d9e0b2a
Them Dang <dnthem@gmail.com>
2025-03-12 18:09:11 -0700

    change Number to Double

--- ttl-store/db/models/Service.ts
+++ ttl-store/db/models/Service.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const serviceSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: mongoose.Schema.Types.Double, required: true },
   currencyType: { type: String, required: true },
   provider: { type : String, required: true },
   max_users: { type: Number, required: true },
