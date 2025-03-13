e0839253b9b6bf493bac392c261f5f67c47b829b
Them Dang <dnthem@gmail.com>
2025-03-12 18:09:49 -0700

    change Number to Double

--- ttl-store/db/models/Payment.ts
+++ ttl-store/db/models/Payment.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const paymentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   poolId: { type: mongoose.Schema.Types.ObjectId, ref: "Pool", required: true },
-  amount: { type: Number, required: true },
+  amount: { type: mongoose.Schema.Types.Double, required: true },
   status: { type: String, required: true },
 }, { timestamps: true });
 
